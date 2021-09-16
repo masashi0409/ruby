c23ff871c1b53f84e647b478a51c1ada20530225
ShimanoMasashi <masashi.shimano0409@gmail.com>
2021-09-16 15:42:17 +0000

    ruby

---
+++ calc.rb
@@ -0,0 +1,5 @@
+def calc(num)
+  return "計算できません" if num.zero?
+
+  # 複雑な計算処理
+end
---
+++ lesson7-1.rb
@@ -0,0 +1,6 @@
+puts "2つの数字を入力してください"
+input1 = gets.to_i
+input2 = gets.to_i
+puts "計算結果を出力します"
+puts "a*b=#{input1 * input2}"
+puts "計算を終了します"
